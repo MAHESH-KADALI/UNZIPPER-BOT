73f77453a9992750d03ce862d9781a71abffd01f
MAHESH KADALI <101107025+MAHESH-KADALI@users.noreply.github.com>
2024-05-20 16:43:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,19 +3,19 @@ import os
 
 
 class Config:
-    APP_ID = int(os.environ.get("APP_ID", "4857766"))
-    API_HASH = os.environ.get("API_HASH", "6c3c6facf5598a4b318e138f8c407028")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6810524462:AAFveJiCanlqCrQiAMvQKWGxsAM6VuCE_Vk")
-    LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL", "-1001963446260"))
-    MONGODB_URL = os.environ.get("MONGODB_URL", "mongodb+srv://FILE-TO-LINK:FILE-TO-LINK@cluster0.chpurvy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    BOT_OWNER = int(os.environ.get("BOT_OWNER", "1596559467"))
-    DOWNLOAD_LOCATION = f"{os.path.dirname(_file_)}/Downloaded"
-    THUMB_LOCATION = f"{os.path.dirname(_file_)}/Thumbnails"
+    APP_ID = int(os.environ.get("APP_ID"))
+    API_HASH = os.environ.get("API_HASH")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    LOGS_CHANNEL = int(os.environ.get("LOGS_CHANNEL"))
+    MONGODB_URL = os.environ.get("MONGODB_URL")
+    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
+    DOWNLOAD_LOCATION = f"{os.path.dirname(__file__)}/Downloaded"
+    THUMB_LOCATION = f"{os.path.dirname(__file__)}/Thumbnails"
     TG_MAX_SIZE = 2097152000
     MAX_MESSAGE_LENGTH = 4096
     # Default chunk size (0.005 MB → 1024*6) Increase if you need faster downloads
     CHUNK_SIZE = 1024 * 1024 * 10  # 10 MB
-    BOT_THUMB = f"{os.path.dirname(_file_)}/bot_thumb.jpg"
+    BOT_THUMB = f"{os.path.dirname(__file__)}/bot_thumb.jpg"
     MAX_CONCURRENT_TASKS = 75
     MAX_TASK_DURATION_EXTRACT = 45 * 60  # 45 minutes (in seconds)
     MAX_TASK_DURATION_MERGE = 90 * 60  # 1 hour 30 minutes (in seconds)
